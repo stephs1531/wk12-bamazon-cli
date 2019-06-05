625f1426a80f589ea638b4e81637d66ab2eed65f
Stephanie Sapp <stephs1531@gmail.com>
2019-06-04 22:36:46 -0500

    got table and 1st inquirer prompt working

--- app.js
+++ app.js
@@ -2,17 +2,18 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+//experiment with cli-table to make a prettier looking table
+var Table = require("cli-table");
+var table = new Table ({
+  head: ["Item Id", "Item", "Department", "Price", "Quantity"],
+  colWidths: [20, 40, 15 ,15, 15]
+});
+
 //Create connection to database
 var connection = mysql.createConnection({
     host: "localhost",
-  
-    // Your port; if not 3306
     port: 3306,
-  
-    // Your username
     user: "root",
-  
-    // Your password
     password: "Ginger998!",
     database: "bamazon_db"
   });
@@ -21,34 +22,65 @@ var connection = mysql.createConnection({
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     // console.table(res);
-   
-    start();
   });
 
-  //start function with inquirer prompts
-  function start() {
-    
-    //function to console.table products
-    var query = "SELECT * FROM products";
-    connection.query(query, function(err, res) {
-        console.table(res);
-      });
+  //start shopping function
+  shop();
+
+  function shop() {
+    connection.query("SELECT * FROM products", function (err, res) {
       
-    // inquirer prompts
-      inquirer
-        .prompt([
-            {
-          name: "itemID",
-          type: "input",
-          message: "What is the id of the item you wish to buy?",
-            },
-            {
-            name: "quantity",
-            type: "number",
-            message: "How many do you want?"
-            }
-        ])
-        .then(function(answer) {
-         console.log(answer);
-        });
-     };
+      //add rows from database to cli-table
+      for (var i = 0; i<res.length; i++) {
+        table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price.toFixed(2), res[i].stock_quantity])
+      }
+      console.log(table.toString());
+
+        //inquirer prompts to ask user what they want to buy and how much
+      inquirer.prompt([{
+        name: "choice",
+        type: "list",
+        message: "What do you want to buy?",
+        choices: function (value) {
+          //pull choices from database response from our query and put them into a new array to choose from
+          var choices = [];
+          for (var i = 0; i < res.length; i++) {
+            choices.push(res[i].product_name);
+          }
+          return choices;
+        }
+      }]); //close 1st inquirer prompt
+
+    }); //close first connection.query
+
+  
+  };
+
+
+  //start function with inquirer prompts
+  // function start() {
+    
+  //   //function to console.table products
+  //   var query = "SELECT * FROM products";
+  //   connection.query(query, function(err, res) {
+  //       console.table(res);
+  //     });
+      
+  //   // inquirer prompts
+  //     inquirer
+  //       .prompt([
+  //           {
+  //         name: "itemID",
+  //         type: "input",
+  //         message: "What is the id of the item you wish to buy?",
+  //           },
+  //           {
+  //           name: "quantity",
+  //           type: "number",
+  //           message: "How many do you want?"
+  //           }
+  //       ])
+  //       .then(function(answer) {
+  //        console.log(answer);
+  //       });
+  //    };
